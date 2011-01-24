3070afdc490528d8ceebb43d2824886c2b1fa680
Antonin Perrot-Audet <antonin07130@gmail.com>
2011-01-24 14:30:31 +0100

    modified executables accordingly

--- Exe/Initialize.cxx
+++ Exe/Initialize.cxx
@@ -6,17 +6,7 @@
 #include "itkImageFileReader.h"
 #include "itkImageFileWriter.h"
 #include "itkRescaleIntensityImageFilter.h"
-
-#include "itkVector.h"
-#include "itkArray.h"
-#include "itkEuclideanDistanceMetric.h"
-
-
-
-#include "itkImageRegionIteratorWithIndex.h"
-#include "itkEuclideanDistanceMetric.h"
-
-#include "math.h"
+#include "itkInitialSubjectiveSurfaceSource.h"
 
 
 int main( int argc, char ** argv )
@@ -25,7 +15,7 @@ int main( int argc, char ** argv )
 
   //%%%%%%%%%%%%%%%%%%% ARGUMENTS PARSING %%%%%%%%%%%%%%%%%%%
 
-  if ( argc < 6 )
+  if ( argc < 5 )
     {
     std::cerr << "Missing parameters. " << std::endl;
     std::cerr << "Usage: " << std::endl;
@@ -60,7 +50,7 @@ int main( int argc, char ** argv )
   typedef itk::ImageFileWriter< ImageType >   WriterType;
 
   // simple iterator
-  typedef itk::ImageRegionIteratorWithIndex< ImageType > IteratorType;
+  typedef itk::InitialSubjectiveSurfaceSource< ImageType > SurfaceSourceType;
 
 
 
@@ -79,83 +69,29 @@ int main( int argc, char ** argv )
     std::cout << "ExceptionObject caught !" << std::endl;
     std::cout << err << std::endl;
     return -1;
-    }  
-
-  // create empty image
-  ImageType::Pointer initialImage = ImageType::New();
-  initialImage->SetRegions(reader->GetOutput()->GetLargestPossibleRegion());
-  initialImage->SetSpacing(reader->GetOutput()->GetSpacing());
-  initialImage->Allocate();
-  initialImage->FillBuffer(static_cast<PixelType>(0.));
-
-
-  //n distance metric initialization
-  typedef itk::Array< PixelType > MeasurementVectorType;
-  typedef itk::Statistics::EuclideanDistanceMetric< MeasurementVectorType >
-  DistanceMetricType;
-
-  DistanceMetricType::Pointer distanceMetric = DistanceMetricType::New();
-
-  DistanceMetricType::OriginType originPoint( Dimension );
-  MeasurementVectorType queryPoint( Dimension );
-  originPoint[0] = centerX;
-  originPoint[1] = centerY;
-  originPoint[2] = centerZ;
-
-  distanceMetric->SetOrigin( originPoint );
-
-  // iterator on initial image
-  IteratorType it( initialImage, initialImage->GetRequestedRegion() );
-
-
-
-  if (func == 0) // compute peak function
-    {
-    for ( it.GoToBegin(); !it.IsAtEnd(); ++it )
-      {
-      ImageType::IndexType idx = it.GetIndex();
-      for (unsigned int i = 0; i<Dimension;++i)
-        {
-        queryPoint[i] = idx[i];
-        }
-      it.Set( static_cast<PixelType>
-              (1./(distanceMetric->Evaluate( queryPoint ) + 0.25 )) );
-      }
-    }
-  else // compute revert distance map
-    {
-    PixelType max = 0;
-    PixelType distance;
-    for ( it.GoToBegin(); !it.IsAtEnd(); ++it )
-      {
-      // compute distance from point
-      ImageType::IndexType idx = it.GetIndex();
-      for (unsigned int i = 0; i<Dimension;++i)
-        {
-        queryPoint[i] = idx[i];
-        }
-      distance = distanceMetric->Evaluate( queryPoint );
-      // compute max distance (we could use the fact that it should
-      // be located at the farthest angle of the volume but we may have
-      // non parallepipedic volumes)
-      if (distance > max)
-          max = distance;
-      // negative distance map
-      it.Set( static_cast<PixelType>
-              (-(distance)) );
-      }
-    // get a positive distance map by adding the max distance
-    for ( it.GoToBegin(); !it.IsAtEnd(); ++it )
-      {
-      it.Set( it.Get() + max );
-      }
     }
 
+
+  SurfaceSourceType::Pointer surfaceSource = SurfaceSourceType::New();
+  surfaceSource->SetSize(reader->GetOutput()->GetLargestPossibleRegion().GetSize());
+  surfaceSource->SetSpacing(reader->GetOutput()->GetSpacing());
+  surfaceSource->SetOrigin(reader->GetOutput()->GetOrigin());
+
+  ImageType::PointType focusPoint;
+  focusPoint[0]= centerX;
+  focusPoint[1]= centerY;
+  focusPoint[2]= centerZ;
+
+
+  surfaceSource->SetFunction( func ); //peak function
+  surfaceSource->SetFocusPoint(focusPoint);
+  // Run the pipeline
+  surfaceSource->Update();
 
   // write output
   WriterType::Pointer writer = WriterType::New();
   writer->SetFileName( argv[6] );
-  writer->SetInput(initialImage);
+  writer->SetInput(surfaceSource->GetOutput());
   try
     {
     writer->Update();
